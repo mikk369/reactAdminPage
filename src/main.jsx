4a90ee05c22030516a0a4fd5357e80676e716154
Mike <jogdial25@gmail.com>
2024-12-13 10:55:50 +0200

    removed unnessecary files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GetAllBookings from './GetAllBookings.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('custom-admin-page-root')).render(
   <React.StrictMode>
